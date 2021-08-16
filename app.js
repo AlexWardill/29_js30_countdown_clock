24121121d9db0c26b343360ba79f03877c7f95c9
Alex Wardill <alexander@wardill.net>
2021-08-16 19:13:03 +0100

    initialised js

---
+++ app.js
@@ -0,0 +1,6 @@
+const navItems = [...document.querySelectorAll('set-time')];
+const userInput = document.querySelector('#enter-time');
+
+const countdownTime = document.querySelector('#countdown');
+const returnTime = document.querySelector('#return-time');
+
