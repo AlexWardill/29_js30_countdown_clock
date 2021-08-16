72155de0562b8eb869113e9a2b473603f6d921a0
Alex Wardill <alexander@wardill.net>
2021-08-16 22:00:18 +0100

    updated javascript

--- app.js
+++ app.js
@@ -8,14 +8,32 @@ const returnTime = document.querySelector('#return-time');
 
 navItems.forEach(item => item.addEventListener('click', (e) => {
     countdownTime.innerText = e.target.dataset.time;
+    // decrement time
+        // get current time every second
+        // find the diference between current event time
+        // when it gets to 0, stop. (use while loop)
+    let time_then = getCurrentTime();
+    return time_then;
+}));
 
+
+while (countdownTime !== '' || countdownTime !== '00:00') {
+    updateClock();
+}
+
+
+function getCurrentTime() {
     let today = new Date();
     let s_then = today.getSeconds();
     let m_then = today.getMinutes();
     let h_then = today.getHours();
+    let timeData = {'seconds' : s_then, 'minutes' : m_then, 'hours' : h_then};
+    return timeData;
+}
 
-    let elapsed = today.getSeconds() - s_then;
-    console.log(elapsed);
-}));
+
+function updateClock() {
+    time_now = setInterval()
+}
 
 
