f8f47af1862821d12ec6849b0632b0d4479f4554
Alex Wardill <alexander@wardill.net>
2021-08-16 19:54:57 +0100

    first javascript

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
-const navItems = [...document.querySelectorAll('set-time')];
+const navItems = [...document.querySelectorAll('.set-time')];
 const userInput = document.querySelector('#enter-time');
+const body = document.querySelector('body');
 
 const countdownTime = document.querySelector('#countdown');
 const returnTime = document.querySelector('#return-time');
+
+
+navItems.forEach(item => item.addEventListener('click', (e) => {
+    countdownTime.innerText = e.target.dataset.time;
+
+    let today = new Date();
+    let s_then = today.getSeconds();
+    let m_then = today.getMinutes();
+    let h_then = today.getHours();
+
+    let elapsed = today.getSeconds() - s_then;
+    console.log(elapsed);
+}));
+
 
