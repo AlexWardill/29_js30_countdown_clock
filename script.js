08735d1431787fcfe08c06004bbe605a15e92c44
Alex Wardill <alexander@wardill.net>
2021-08-17 14:35:34 +0100

    finished 1

--- script.js
+++ script.js
@@ -11,8 +11,14 @@ countdownTime.innerText = '00:00';
 navItems.forEach(item => item.addEventListener('click', (e) => timer(e.target.dataset.time)));
 
 function timer(seconds) {
+    displayTimeLeft(seconds);
+    clearInterval(countdown);
+
     const now = Date.now();
     const then = now + seconds * 1000;
+
+    displayEndTime(then);
+    
     countdown = setInterval(() => {
         const remaining = Math.round((then - Date.now()) / 1000);
         if (remaining < 0) {
@@ -24,26 +30,27 @@ function timer(seconds) {
 }
 
 function displayTimeLeft(remaining) {
-    console.log(remaining);
     
     let hours = Math.floor(remaining / 3600).toString();
-    let minutes = Math.floor(remaining / 60).toString();
+    let minutes = Math.floor(remaining / 60 % 60).toString();
     let seconds = (remaining % 60).toString();
-    console.log(`${hours} : ${minutes} : ${seconds}`);
+    //console.log(`${hours} : ${minutes} : ${seconds}`);
     
+    // THIS IS SO UGLY!!
     if (hours == 0) {
         hours = ''; 
     } else if (hours < 10) {
         hours = '0' + hours + ':';
-    } else {hours += ';'} 
+    } else {hours += ':'} 
     if (minutes < 10) {
         minutes = '0' + minutes;
+
     } if (seconds < 10) {
         seconds = '0' + seconds;
     }
     
     let finalTime = hours + minutes + ':' + seconds;
-    
+    document.title = finalTime;
     countdownTime.innerText = finalTime;
 }
 
@@ -56,3 +63,12 @@ userInput.addEventListener('change', (e) => {
     }
     timer(e.target.value * 60);
 })
+
+
+function displayEndTime(timestamp) {
+    const thenDate = new Date(timestamp);
+    const thenHours = thenDate.getHours();
+    const thenMinutes = thenDate.getMinutes()
+    returnTime.innerText = `Be back here at ${thenHours}:${thenMinutes}`;
+}
+
